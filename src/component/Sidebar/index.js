a5a0747c19d35ab8f79c98be81659ab0b032b8aa
nambirajan <codemantra@ip-192-168-1-97.ec2.internal>
2022-09-23 17:02:38 +0530

    Initial commit

---
+++ src/component/Sidebar/index.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Sidebar = ()=>{
+    return <><div>
+        <Link>
+        <img src={} alt=""/>
+        </Link></div></>
+}
+
+export default Sidebar
